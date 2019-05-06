3929df962676f9d1f254571dc44da0732482a2b0
Leonardo Lucas Dos Santos <sombratwo@localhost.localdomain>
2019-05-05 23:23:31 -0300

    ajuste

--- src/Core/Routes.ts
+++ src/Core/Routes.ts
@@ -3,6 +3,8 @@ import { injectable, inject } from 'inversify'
 import Server from '../Ports/Server'
 import { Request, Response } from '../Ports/http'
 import { ServerType } from '../server/types'
+import morgan from 'morgan'
+import helmet from 'helmet'
 
 @injectable()
 export default class Routes implements Application {
@@ -15,6 +17,8 @@ export default class Routes implements Application {
   }
 
   public createRoutes (): void {
+    this.server.use(morgan('dev'))
+    this.server.use(helmet())
     this.server.get('/', (req: Request, res: Response):void => {
       res.send('asaaa')
     })
--- src/server/init.ts
+++ src/server/init.ts
@@ -3,16 +3,11 @@
 import dotenv from 'dotenv'
 import { serverStart } from '../Helpers'
 import application from './container'
-import helmet from 'helmet'
-import morgan from 'morgan'
 
 dotenv.config()
 
 const { SERVER_PORT } = process.env
 
 application.createRoutes()
-
-application.server.use(morgan('dev'))
-application.server.use(helmet())
 
 application.server.listen(SERVER_PORT, (): void => serverStart(SERVER_PORT))
